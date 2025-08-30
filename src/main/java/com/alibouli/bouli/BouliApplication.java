7c273f1401a4f082ed135f222e8ace8208ccecc8
Amit Chauhan <amitkc03@gmail.com>
2025-08-30 08:40:12 +0530

    commit 1

---
+++ src/main/java/com/alibouli/bouli/BouliApplication.java
@@ -0,0 +1,13 @@
+package com.alibouli.bouli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alibouli/bouli/BouliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibouli.bouli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
