ba5b8dcc19c4b55cf1ea51a60b16054ea85b7f3f
Amit Chauhan <amitkc03@gmail.com>
2025-08-30 09:03:38 +0530

    commit 2

---
+++ src/main/java/com/alibouli/bouli/TestClass.java
@@ -0,0 +1,18 @@
+package com.alibouli.bouli;
+
+public class TestClass {
+
+	public TestClass() {
+		super();
+	}
+	
+int age;
+int code;
+
+public void add() {
+	
+}
+	public void name() {
+		
+	}
+}
